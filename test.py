679acad107c1487e6d9c3b649828e05d1a6608d1
itgsod-Timothy-Holmsten <timothy.holmsten@elev.it-gymnasiet.se>
2015-05-06 10:09:10 +0200

    Changes

--- test.py
+++ test.py
@@ -1,18 +1,34 @@
 from reddit import client
 from reddit.user import User
 from reddit.reddits import Subreddit
+from reddit.utils import Utils
+from reddit.post import Post, Reply, Comment
 
 tim = client.login('timsbot')
-
+from pprint import pprint
 print tim.link_karma()
 
-tim.submit("timsbot", "self", "A submit", "123 Test")
+#tim.submit("timsbot", "self", "A submit", "123 Test")
 
 
 
 
+sub = Subreddit("timsbot")
+post = Post("timsbot", "31ggml")
 
+#print post.comments()[1]['data']['children'][1]['data']['replies']['data']['children'][0]
 
+#rep = Reply(post.comments()[1]['data']['children'][1]['data']['replies']['data']['children'][0])
+
+#rep.print_replies(rep)
+
+#print post.comments()[1]['data']['children'][0]
+
+#print post.comments()[1]['data']['children'][1]['data']
+
+com = Reply(post.comments()[1]['data']['children'][1])
+
+print com.print_replies(com)
 
 
 
